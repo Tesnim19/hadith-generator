afd108b7ba00225b994a94c62a06857754f0cdce
Tesnim19 <tesnimabdi67@gmail.com>
2024-02-28 23:16:12 +0300

    Initial commit: Set up React project and created HadithComponent

---
+++ src/HadithComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HadithComponent = () => {
+  return (
+    <div>
+      <h1>Today's Hadith</h1>
+      <p>This is a placeholder for the Hadith content.</p>
+    </div>
+  );
+};
+
+export default HadithComponent;
