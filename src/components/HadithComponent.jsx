f887087c4367cf05511fa10f2256d2ccbd75c978
Tesnim19 <tesnimabdi67@gmail.com>
2024-04-01 00:22:38 +0300

    fixing bugs

--- src/components/HadithComponent.jsx
+++ src/components/HadithComponent.jsx
@@ -1,86 +1,96 @@
 import React, { useState, useEffect } from 'react';
 import '../styles/HadithComponent.css';
 
-const dummyHadiths = [
-  {
-    id: 1,
-    text: "The best of you are those who learn the Quran and teach it to others.",
-    reference: "Sahih Bukhari"
-  },
-  {
-    id: 2,
-    text: "Do not be people without minds of your own, saying that if others treat you well you will treat them well, and that if they do wrong you will do wrong. Instead, accustom yourselves to do good if people do good and not to do wrong if they do evil.",
-    reference: "Sunan At-Tirmidhi"
-  },
-  {
-    id: 3,
-    text: "The strong person is not the one who can wrestle someone else down. The strong person is the one who can control himself when he is angry.",
-    reference: "Sahih al-Bukhari"
-  },
-  {
-    id: 4,
-    text: "Kindness is a mark of faith, and whoever has not kindness has not faith.",
-    reference: "Sahih Muslim"
-  },
-  {
-    id: 5,
-    text: "A person will not be a believer until his heart becomes obedient to Allah.",
-    reference: "Musnad Ahmad"
-  }
-];
-
 const HadithComponent = () => {
   const [hadith, setHadith] = useState(null);
   const [isFavorite, setIsFavorite] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
-  const [recentHadiths, setRecentHadiths] = useState([]);
+  const [recentlyViewed, setRecentlyViewed] = useState([]);
+  const [favorites, setFavorites] = useState([]);
 
   useEffect(() => {
     fetchRandomHadith();
   }, []);
 
-  const fetchRandomHadith = () => {
+  const fetchRandomHadith = async () => {
     setIsLoading(true);
     try {
-      const randomIndex = Math.floor(Math.random() * dummyHadiths.length);
-      const randomHadith = dummyHadiths[randomIndex];
-      setHadith(randomHadith);
-      setIsFavorite(false);
-      setIsLoading(false);
+      // Dummy Hadiths
+      const dummyHadiths = [
+        {
+          id: 1,
+          text: "The best among you are those who have the best manners and character.",
+          narrator: "Sahih Bukhari",
+        },
+        {
+          id: 2,
+          text: "The most perfect believer in faith is the one whose character is finest and who is kindest to his wife.",
+          narrator: "Tirmidhi",
+        },
+        {
+          id: 3,
+          text: "Speak good or remain silent.",
+          narrator: "Sahih Bukhari",
+        }
+      ];
+
+      // Randomly select a Hadith
+      const randomHadith = dummyHadiths[Math.floor(Math.random() * dummyHadiths.length)];
       
-      // Add the current Hadith to the list of recent Hadiths
-      setRecentHadiths(prevHadiths => [randomHadith, ...prevHadiths]);
+      // Set the fetched Hadith
+      setHadith(randomHadith);
+      setIsFavorite(favorites.some(item => item.id === randomHadith.id));
+      setIsLoading(false);
+      viewQuote({ ...randomHadith }); // View the newly fetched Hadith (clone to prevent reference sharing)
     } catch (error) {
       setError('Error fetching Hadith. Please try again later.');
       setIsLoading(false);
     }
   };
 
-  const toggleFavorite = (id) => {
-    const updatedHadiths = recentHadiths.map(hadith => {
-      if (hadith.id === id) {
-        return { ...hadith, isFavorite: !hadith.isFavorite };
+  const toggleFavorite = (quote) => {
+    if (quote) {
+      const isFavorited = favorites.some(item => item.id === quote.id);
+      setIsFavorite(!isFavorited);
+      if (isFavorited) {
+        setFavorites(favorites.filter(item => item.id !== quote.id));
+      } else {
+        setFavorites([...favorites, { ...quote }]); // Clone to prevent reference sharing
       }
-      return hadith;
-    });
-    setRecentHadiths(updatedHadiths);
+    }
   };
 
-  const removeHadith = (id) => {
-    const filteredHadiths = recentHadiths.filter(hadith => hadith.id !== id);
-    setRecentHadiths(filteredHadiths);
+  const removeQuote = (quote) => {
+    if (quote) {
+      setRecentlyViewed(recentlyViewed.filter(item => item.id !== quote.id));
+    }
+  };
+
+  const shareHadith = () => {
+    // Implement sharing functionality here
+  };
+
+  const viewQuote = (quote) => {
+    if (quote) {
+      if (!recentlyViewed.some(item => item.id === quote.id)) {
+        setRecentlyViewed([quote, ...recentlyViewed.slice(0, 3)]); // Limit to 3 recently viewed Hadiths
+      }
+    }
   };
 
   return (
     <div className="hadith-container">
-      <div className="hadith-card">
+      <div className="main-card hadith-card">
         <div className="hadith-header">
           <h1 className="hadith-title">Today's Hadith</h1>
           {hadith && (
             <div>
-              <button className={`favorite-button ${isFavorite ? 'favorited' : ''}`} onClick={() => setIsFavorite(!isFavorite)}>
-                <i className={`fas fa-heart ${isFavorite ? 'favorited' : ''}`}></i>
+              <button className={`favorite-button ${isFavorite ? 'favorited' : ''}`} onClick={() => toggleFavorite(hadith)}>
+                {isFavorite ? 'Remove from Favorites' : 'Add to Favorites'}
+              </button>
+              <button className="share-button" onClick={shareHadith}>
+                Share
               </button>
             </div>
           )}
@@ -89,38 +99,52 @@ const HadithComponent = () => {
           <p className="loading-message">Loading...</p>
         ) : error ? (
           <p className="error-message">{error}</p>
-        ) : hadith ? (
-          <div className="hadith-content">
-            <>
-              <p className="hadith-text">{hadith.text}</p>
-              <p className="hadith-reference">- {hadith.reference}</p>
-            </>
-          </div>
         ) : (
-          <p className="no-hadith-message">Press 'Generate Hadith' to get a new Hadith.</p>
+          <div className="hadith-content">
+            {hadith ? (
+              <>
+                <p className="hadith-text">{hadith.text}</p>
+                <p className="hadith-reference">- {hadith.narrator}</p>
+              </>
+            ) : (
+              <p className="no-hadith-message">Press 'Generate Hadith' to get a new Hadith.</p>
+            )}
+          </div>
         )}
         <button className="generate-button" onClick={fetchRandomHadith}>Generate Hadith</button>
       </div>
-      <div className="recent-hadiths">
-        <h2>Recently Viewed Hadiths</h2>
-        <ul>
-          {recentHadiths.map((hadith, index) => (
-            <li key={hadith.id}>
-              <div className="recent-hadith-content">
-                <div className="recent-hadith-header">
-                  <button className={`favorite-button ${hadith.isFavorite ? 'favorited' : ''}`} onClick={() => toggleFavorite(hadith.id)}>
-                    <i className={`fas fa-heart ${hadith.isFavorite ? 'favorited' : ''}`}></i>
-                  </button>
-                  <button className="remove-button" onClick={() => removeHadith(hadith.id)}>
-                    <i className="fas fa-times"></i>
-                  </button>
-                </div>
-                <p className="recent-hadith-text">{hadith.text}</p>
-                <p className="recent-hadith-reference">- {hadith.reference}</p>
+
+      <div className="side-cards">
+        <div className="recent-hadiths">
+          <h2>Recently Viewed</h2>
+          {recentlyViewed.map((quote, index) => (
+            <div key={index} className="hadith-card">
+              <div className="hadith-card-content">
+                <p className="hadith-text">{quote.text}</p>
+                <p className="hadith-reference">- {quote.narrator}</p>
               </div>
-            </li>
+              <button className="remove-button" onClick={() => removeQuote(quote)}>Remove</button>
+              <button className="favorite-button" onClick={() => toggleFavorite(quote)}>
+                {favorites.some(item => item.id === quote.id) ? 'Remove from Favorites' : 'Add to Favorites'}
+              </button>
+            </div>
           ))}
-        </ul>
+        </div>
+        <div className="favorite-hadiths">
+          <h2>Favorites</h2>
+          {favorites.map((quote, index) => (
+            <div key={index} className="hadith-card">
+              <div className="hadith-card-content">
+                <p className="hadith-text">{quote.text}</p>
+                <p className="hadith-reference">- {quote.narrator}</p>
+              </div>
+              <button className="remove-button" onClick={() => removeQuote(quote)}>Remove</button>
+              <button className="favorite-button" onClick={() => toggleFavorite(quote)}>
+                {favorites.some(item => item.id === quote.id) ? 'Remove from Favorites' : 'Add to Favorites'}
+              </button>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
