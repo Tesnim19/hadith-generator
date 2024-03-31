a107341d3a6ba7721734ac1530066f2a54ca6c8a
Tesnim19 <tesnimabdi67@gmail.com>
2024-03-31 23:51:43 +0300

    add favorite and recently viewed

--- src/components/HadithComponent.jsx
+++ src/components/HadithComponent.jsx
@@ -1,93 +1,127 @@
 import React, { useState, useEffect } from 'react';
 import '../styles/HadithComponent.css';
 
+const dummyHadiths = [
+  {
+    id: 1,
+    text: "The best of you are those who learn the Quran and teach it to others.",
+    reference: "Sahih Bukhari"
+  },
+  {
+    id: 2,
+    text: "Do not be people without minds of your own, saying that if others treat you well you will treat them well, and that if they do wrong you will do wrong. Instead, accustom yourselves to do good if people do good and not to do wrong if they do evil.",
+    reference: "Sunan At-Tirmidhi"
+  },
+  {
+    id: 3,
+    text: "The strong person is not the one who can wrestle someone else down. The strong person is the one who can control himself when he is angry.",
+    reference: "Sahih al-Bukhari"
+  },
+  {
+    id: 4,
+    text: "Kindness is a mark of faith, and whoever has not kindness has not faith.",
+    reference: "Sahih Muslim"
+  },
+  {
+    id: 5,
+    text: "A person will not be a believer until his heart becomes obedient to Allah.",
+    reference: "Musnad Ahmad"
+  }
+];
+
 const HadithComponent = () => {
   const [hadith, setHadith] = useState(null);
-  const [chapter, setChapter] = useState(null);
   const [isFavorite, setIsFavorite] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [recentHadiths, setRecentHadiths] = useState([]);
 
   useEffect(() => {
     fetchRandomHadith();
   }, []);
 
-  const fetchRandomHadith = async () => {
+  const fetchRandomHadith = () => {
     setIsLoading(true);
     try {
-      // Fetch the list of books
-      const booksResponse = await fetch('https://www.hadithapi.com/api/books?apiKey=$2y$10$LRtmSYoqezCWBRryl1i9eTeqlpyPqTNuhz6KyCVkB3ExvNlrCWiG');
-      const booksData = await booksResponse.json();
-  
-      // Randomly select a book from the list
-      const randomBook = booksData.books[Math.floor(Math.random() * booksData.books.length)];
-      
-      // Fetch the chapters for the selected book
-      const chaptersResponse = await fetch(`https://www.hadithapi.com/api/${randomBook.slug}/chapters?apiKey=$2y$10$LRtmSYoqezCWBRryl1i9eTeqlpyPqTNuhz6KyCVkB3ExvNlrCWiG`);
-      const chaptersData = await chaptersResponse.json();
-      
-      // Randomly select a chapter from the list
-      const randomChapter = chaptersData.chapters[Math.floor(Math.random() * chaptersData.chapters.length)];
-      
-      // Fetch a random Hadith from the selected book and chapter
-      const hadithResponse = await fetch(`https://www.hadithapi.com/api/hadiths?apiKey=$2y$10$LRtmSYoqezCWBRryl1i9eTeqlpyPqTNuhz6KyCVkB3ExvNlrCWiG&book=${randomBook.slug}&chapter=${randomChapter.chapter_number}`);
-      const hadithData = await hadithResponse.json();
-      
-      // Randomly select a Hadith from the list
-      const randomHadith = hadithData[Math.floor(Math.random() * hadithData.length)];
-      
-      // Set the fetched Hadith and chapter
+      const randomIndex = Math.floor(Math.random() * dummyHadiths.length);
+      const randomHadith = dummyHadiths[randomIndex];
       setHadith(randomHadith);
-      setChapter(randomChapter);
-      setIsFavorite(false); // Reset favorite status when new Hadith is fetched
+      setIsFavorite(false);
       setIsLoading(false);
+      
+      // Add the current Hadith to the list of recent Hadiths
+      setRecentHadiths(prevHadiths => [randomHadith, ...prevHadiths]);
     } catch (error) {
       setError('Error fetching Hadith. Please try again later.');
       setIsLoading(false);
     }
   };
 
-  const toggleFavorite = () => {
-    setIsFavorite(!isFavorite);
+  const toggleFavorite = (id) => {
+    const updatedHadiths = recentHadiths.map(hadith => {
+      if (hadith.id === id) {
+        return { ...hadith, isFavorite: !hadith.isFavorite };
+      }
+      return hadith;
+    });
+    setRecentHadiths(updatedHadiths);
   };
 
-  const shareHadith = () => {
-    // Implement sharing functionality here
+  const removeHadith = (id) => {
+    const filteredHadiths = recentHadiths.filter(hadith => hadith.id !== id);
+    setRecentHadiths(filteredHadiths);
   };
 
   return (
-    <div className="hadith-card">
-      <div className="hadith-header">
-        <h1 className="hadith-title">Today's Hadith</h1>
-        {hadith && (
-          <div>
-            <button className={`favorite-button ${isFavorite ? 'favorited' : ''}`} onClick={toggleFavorite}>
-              <i className={`fas fa-heart ${isFavorite ? 'favorited' : ''}`}></i>
-            </button>
-            <button className="share-button" onClick={shareHadith}>
-              <i className="fas fa-share"></i>
-            </button>
-          </div>
-        )}
-      </div>
-      {isLoading ? (
-        <p className="loading-message">Loading...</p>
-      ) : error ? (
-        <p className="error-message">{error}</p>
-      ) : (
-        <div className="hadith-content">
-          {hadith ? (
+    <div className="hadith-container">
+      <div className="hadith-card">
+        <div className="hadith-header">
+          <h1 className="hadith-title">Today's Hadith</h1>
+          {hadith && (
+            <div>
+              <button className={`favorite-button ${isFavorite ? 'favorited' : ''}`} onClick={() => setIsFavorite(!isFavorite)}>
+                <i className={`fas fa-heart ${isFavorite ? 'favorited' : ''}`}></i>
+              </button>
+            </div>
+          )}
+        </div>
+        {isLoading ? (
+          <p className="loading-message">Loading...</p>
+        ) : error ? (
+          <p className="error-message">{error}</p>
+        ) : hadith ? (
+          <div className="hadith-content">
             <>
               <p className="hadith-text">{hadith.text}</p>
               <p className="hadith-reference">- {hadith.reference}</p>
-              <p className="chapter-info">Chapter: {chapter.name}</p>
             </>
-          ) : (
-            <p className="no-hadith-message">Press 'Generate Hadith' to get a new Hadith.</p>
-          )}
-        </div>
-      )}
-      <button className="generate-button" onClick={fetchRandomHadith}>Generate Hadith</button>
+          </div>
+        ) : (
+          <p className="no-hadith-message">Press 'Generate Hadith' to get a new Hadith.</p>
+        )}
+        <button className="generate-button" onClick={fetchRandomHadith}>Generate Hadith</button>
+      </div>
+      <div className="recent-hadiths">
+        <h2>Recently Viewed Hadiths</h2>
+        <ul>
+          {recentHadiths.map((hadith, index) => (
+            <li key={hadith.id}>
+              <div className="recent-hadith-content">
+                <div className="recent-hadith-header">
+                  <button className={`favorite-button ${hadith.isFavorite ? 'favorited' : ''}`} onClick={() => toggleFavorite(hadith.id)}>
+                    <i className={`fas fa-heart ${hadith.isFavorite ? 'favorited' : ''}`}></i>
+                  </button>
+                  <button className="remove-button" onClick={() => removeHadith(hadith.id)}>
+                    <i className="fas fa-times"></i>
+                  </button>
+                </div>
+                <p className="recent-hadith-text">{hadith.text}</p>
+                <p className="recent-hadith-reference">- {hadith.reference}</p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
